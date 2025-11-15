f002e0e524ff06e9def7ea5eb339213d714e44f9
SbiiAmine <sbii.amine@esprit.tn>
2025-11-15 10:06:50 +0100

    Add files via upload

--- TestListSleam1/TestListSleam1/src/Test.java
+++ TestListSleam1/TestListSleam1/src/Test.java
@@ -22,24 +22,22 @@ public class Test {
         Collections.sort(strings);
         System.out.println("Strings tri croissant " + strings);
         Collections.sort(strings, Collections.reverseOrder());
-        System.out.println("Strings tri decroissant " + strings);
+        System.out.println("Strings tri decroissant" + strings);
 
-        // --- Liste de Personne ---
-        Personne p1 = new Personne(1, "Dupont", "Jean", 30);
-        Personne p2 = new Personne(2, "Martin", "Alice", 25);
-        Personne p3 = new Personne(3, "Bernard", "Paul", 40);
-        Personne p4 = new Personne(4, "Durand", "Sophie", 30);
+        Personne p1 = new Personne(1, "amine", "1", 23);
+        Personne p2 = new Personne(2, "amine", "2", 25);
+        Personne p3 = new Personne(3, "amine", "3", 27);
+
 
         List<Personne> personnes = new ArrayList<>();
         personnes.add(p1);
         personnes.add(p2);
         personnes.add(p3);
-        personnes.add(p4);
 
         personnes.sort(Comparator.comparingInt(Personne::getAge));
-        System.out.println("croissant : " + personnes);
+        System.out.println("croissant " + personnes);
 
         personnes.sort(Comparator.comparingInt(Personne::getAge).reversed());
-        System.out.println("decroissant : " + personnes);
+        System.out.println("decroissant" + personnes);
     }
 }
